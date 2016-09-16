89b8da9024ae7e1e9c5d2936e240bcafb13d1e40
Joeri Hermans <joeri@joerihermans.com>
2016-09-17 00:56:51 +0200

    Implement CrashEventListener

--- server/server.py
+++ server/server.py
@@ -8,6 +8,8 @@ from Queue import Queue
 
 import time
 
+import numpy as np
+
 import json
 
 ## BEGIN Event definitions. ####################################################
@@ -39,8 +41,75 @@ class Event(object):
 
 class EventListener(object):
 
-    def handle(self):
+    def handle(self, update):
         raise NotImplementedError
+
+class CrashEventListener(object):
+
+    def __init__(self, crash_tolerance=4,
+                 event_timeout=20):
+        self.crash_tolerance = crash_tolerance
+        self.event_timeout = event_timeout
+
+    def contains_arguments(self, update):
+        contains = True
+        contains &= ("acceleration_x" in update["sensors"])
+        contains &= ("acceleration_y" in update["sensors"])
+        contains &= ("acceleration_z" in update["sensors"])
+
+        return contains
+
+    def create_crash_event(self, update, magnitude):
+        # Create the attributes which are required to describe the event.
+        event_type = "crash"
+        timestamp = float(update["update_timestamp"])
+        expiration_timestamp = timestamp + self.event_timeout
+        car_id = int(update["car_id"])
+        latitude = float(update["sensors"]["latitude"])
+        longitude = float(update["sensors"]["longitude"])
+        # Build an event using a dictionary.
+        event = {}
+        event["event_type"] = event_type
+        event["timestamp"] = timestamp
+        event["expiration_timestamp"] = expiration_timestamp
+        event["car_id"] = car_id
+        event["latitude"] = latitude
+        event["longitude"] = longitude
+
+        return event
+
+
+    def analyse_update(self, update):
+        # Fetch the parameters from the sensor readings.
+        a_x = float(update["sensors"]["acceleration_x"])
+        a_y = float(update["sensors"]["acceleration_y"])
+        a_z = float(update["sensors"]["acceleration_z"])
+        # Compute the magnitude.
+        vector = np.asarray([a_x, a_y, a_z])
+        powers = np.asarray([2, 2, 2])
+        vector = np.power(vector, powers)
+        sum = 0.0
+        for v_i in vector:
+            sum += v_i
+        magnitude = np.sqrt(sum)
+        # Check if the magnitude hits the crash tolerance.
+        if magnitude > self.crash_tolerance:
+            event = self.create_crash_event(update, magnitude)
+        else:
+            event = None
+
+        return event
+
+
+    def handle(self, update):
+        event = None
+
+        # Check if the update has the required sensor readings.
+        if self.contains_arguments(update):
+            event = self.analyse_update(update)
+
+        return event
+
 
 ## END Event Listeners. ########################################################
 
@@ -61,9 +130,13 @@ class Application(object):
         with self.mutex_cars:
             self.cars[car_id] = {}
 
+    def add_event(self, event):
+        with self.mutex_events:
+            self.events.append(event)
+
     def add_event_listener(self, event_listener):
         with self.mutex_event_listeners:
-            self.mutex_event_listeners.append(event_listener)
+            self.event_listeners.append(event_listener)
 
     def update_car(self, car_id, data):
         with self.mutex_cars:
@@ -90,7 +163,9 @@ class Application(object):
                 with self.mutex_event_listeners:
                     # Iterate through the event listeners.
                     for listener in self.event_listeners:
-                        pass
+                        event = listener.handle(update)
+                        if not event == None:
+                            self.add_event(event)
             time.sleep(1)
 
     def run(self):
@@ -142,9 +217,13 @@ class Application(object):
 
 def main():
     application = Application()
-    application.add_car(0)
-    application.add_car(1)
-    application.add_car(2)
+    # Add dummy cars (us).
+    application.add_car(0) # Athanos
+    application.add_car(1) # Przemek
+    application.add_car(2) # Joeri
+    # Add event listeners.
+    application.add_event_listener(CrashEventListener(crash_tolerance=4))
+    # Run the application.
     application.run()
 
 ## END Application code. #######################################################
